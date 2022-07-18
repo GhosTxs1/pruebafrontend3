b09a21e9a00d503fe64d28cf9fd958ff305b56dc
Elías Zúñiga <ghostelias77@gmail.com>
2022-07-18 00:06:36 -0400

    dssdfef

--- src/data.jsx
+++ src/data.jsx
@@ -2,10 +2,13 @@ const data = {
     productos: [
         {id: '1',
         nombre: 'poto',
-        precio: 24000},
+        precio: 24000,
+        imagen:''
+        },
         {id: '2',
         nombre: 'pene',
-        precio: 16666
+        precio: 16666,
+        imagen:''
         }
     ]
 }
